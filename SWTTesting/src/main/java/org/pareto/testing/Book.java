e0806512299196a2432722fde112652cf7da027d
heshsum <mail@heshsum.de>
2022-07-03 17:53:26 +0200

    Added Book and TestBook

---
+++ SWTTesting/src/main/java/org/pareto/testing/Book.java
@@ -0,0 +1,269 @@
+package org.pareto.testing;
+
+/**
+ * Class to describe the model of a Book.
+ * <br><code><b>[Softwaretechnik - Aufgabe Testing]</b></code>
+ *
+ * @author Hendrik Schlange (mail@hendrikschlange.de)
+ * @version 1.00 - 15 June 2022 - HS - initial version
+ */
+public class Book {
+    /**
+     * A String representing the title of a book.
+     */
+    private String title;
+
+    /**
+     * A String representing the author of a book.
+     */
+    private String author;
+
+    /**
+     * A String representing the publisher of the book.
+     */
+    private String publisher;
+
+    /**
+     * An integer representing the year of the book.
+     */
+    private int year;
+
+    /**
+     * A String representing the isbn of a book.
+     */
+    private String isbn;
+
+    /**
+     * A double representing the price of a book.
+     */
+    private double price;
+
+    /**
+     * A boolean representing if the book is an e-book.
+     */
+    private boolean ebook;
+
+    /**
+     * Constructor for a new Book object.
+     *
+     * @param title     the title of the book
+     * @param author    the author of the book
+     * @param publisher the publisher of the book
+     * @param year      the year the book was released
+     * @param isbn      the ISBN of the book
+     * @param price     the price of the book
+     * @param ebook     the boolean if the book is an ebook
+     */
+    public Book(String author, String title, String publisher, int year, String isbn, double price, boolean ebook) {
+        this.author = author;
+        this.title = title;
+        this.publisher = publisher;
+        this.year = year;
+        this.isbn = isbn;
+        this.price = price;
+        this.ebook = ebook;
+    }
+
+    /**
+     * Constructor for a new Book object.
+     *
+     * @param title  the title of the book
+     * @param author the author of the book
+     */
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+        this.year = -1;
+        this.price = -1.0;
+    }
+
+    /**
+     * Method to retrieve the author of a book.
+     *
+     * @return the author of the book
+     */
+    public String getAuthor() {
+        return this.author;
+    }
+
+    /**
+     * Method to retrieve the title of a book.
+     *
+     * @return the title of the book
+     */
+    public String getTitle() {
+        return this.title;
+    }
+
+    /**
+     * Method to retrieve the publisher of a book.
+     *
+     * @return the publisher of the book
+     */
+    public String getPublisher() {
+        return this.publisher;
+    }
+
+    /**
+     * Method to retrieve the year of a book.
+     *
+     * @return the year of the book
+     */
+    public int getYear() {
+        return this.year;
+    }
+
+    /**
+     * Method to retrieve the ISBN of a book.
+     *
+     * @return the ISBN of the book
+     */
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    /**
+     * Method to retrieve the price of a book.
+     *
+     * @return the price of the book
+     */
+    public double getPrice() {
+        return this.price;
+    }
+
+    /**
+     * Method to retrieve the information if the book is an ebook.
+     *
+     * @return true if the book is an ebook, false if it is not
+     */
+    public boolean getEbook() {
+        return this.ebook;
+    }
+
+    /**
+     * Method to set the author of a book.
+     *
+     * @param author the author of the book
+     * @throws IllegalArgumentException if the author is blank
+     */
+    public void setAuthor(String author) throws IllegalArgumentException {
+        if (author.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        this.author = author;
+    }
+
+    /**
+     * Method to set the title of a book.
+     *
+     * @param title the title of the book
+     * @throws IllegalArgumentException if the title is blank
+     */
+    public void setTitle(String title) throws IllegalArgumentException {
+        if (title.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        this.title = title;
+    }
+
+    /**
+     * Method to set the publisher of a book.
+     *
+     * @param publisher the publisher of the book
+     */
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    /**
+     * Method to set the year of a book.
+     *
+     * @param year the year of the book
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    /**
+     * Method to set the ISBN of a book.
+     *
+     * @param isbn the ISBN of the book
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * Method to set the price of a book.
+     *
+     * @param price the price of the book
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * Sets the information whether or not a book is an ebook.
+     *
+     * @param ebook the information if the book is an ebook
+     *              True if it the book is an ebook, otherwise false
+     */
+    public void setEbook(boolean ebook) {
+        this.ebook = ebook;
+    }
+
+    /**
+     * Returns a complete textual representation of all attributes set for a Book.
+     *
+     * @return the author and the title of the Book
+     */
+    public String getAllSet() {
+        StringBuilder returnString = new StringBuilder();
+        returnString.append("Author: ").append(this.author).append(System.lineSeparator())
+                .append("Title: ").append(this.title).append(System.lineSeparator());
+
+        if (this.publisher != null) {
+            returnString.append("Publisher: ").append(this.publisher).append(System.lineSeparator());
+        }
+        if (this.year != -1) {
+            returnString.append("Year: ").append(this.year).append(System.lineSeparator());
+        }
+        if (this.isbn != null) {
+            returnString.append("ISBN: ").append(this.isbn).append(System.lineSeparator());
+        }
+        if (this.price != -1.0) {
+            returnString.append("Price: ").append(this.price).append(System.lineSeparator());
+        }
+        returnString.append("Ebook: ").append(this.ebook);
+
+        return returnString.toString();
+    }
+
+    /**
+     * Returns a complete textual representation of a Book.
+     *
+     * @return the author and the title of the Book
+     */
+    public String getAll() {
+        return "Author: " + this.author + System.lineSeparator()
+                + "Title: " + this.title + System.lineSeparator()
+                + "Publisher: " + this.publisher + System.lineSeparator()
+                + "Year: " + this.year + System.lineSeparator()
+                + "ISBN: " + this.isbn + System.lineSeparator()
+                + "Price: " + this.price + System.lineSeparator()
+                + "Ebook: " + this.ebook;
+    }
+
+    /**
+     * Returns a textual representation of author and title of a Book.
+     * If more should be returned, use getAll() or getAllSet()
+     *
+     * @return the author and title of a Book
+     */
+    @Override
+    public String toString() {
+        return "Author: " + author + System.lineSeparator()
+                + "Title: " + title + System.lineSeparator();
+    }
+
+}
---
+++ SWTTesting/src/test/java/org/pareto/testing/TestBook.java
@@ -0,0 +1,155 @@
+package org.pareto.testing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class to test Book.
+ *  <br><code><b>[Softwaretechnik - Aufgabe Testing]</b></code>
+ *
+ *  @author Hendrik Schlange (mail@hendrikschlange.de)
+ *  @version 1.00 - 15 June 2022 - HS - initial version
+ */
+
+class TestBook {
+    /** Book object to use in tests. */
+    Book b;
+
+    /** Default title to use in tests. */
+    String t = "title";
+
+    /** Default author to use in tests. */
+    String a = "author";
+
+    /** Default publisher to use in tests. */
+    String p = "publisher";
+
+    /** Default year to use in tests. */
+    int y = 1988;
+
+    /** Default ISBN to use in tests. */
+    String i = "123456";
+
+    Boolean e = true;
+
+    /** Default price to use in tests. */
+    double price = 9.99;
+
+    @BeforeEach
+    void setup() {
+        b = new Book(a, t);
+    }
+
+    @Test
+    void create() {
+        b.setAuthor(a);
+        b.setTitle(t);
+        b.setPublisher(p);
+        b.setYear(y);
+        b.setIsbn(i);
+        b.setPrice(price);
+        b.setEbook(e);
+
+        Book test = new Book(a, t, p, y, i, price, e);
+
+        Assertions.assertEquals(a, test.getAuthor());
+        Assertions.assertEquals(t, test.getTitle());
+        Assertions.assertEquals(p, test.getPublisher());
+        Assertions.assertEquals(y, test.getYear());
+        Assertions.assertEquals(i, test.getIsbn());
+        Assertions.assertEquals(price, test.getPrice());
+        Assertions.assertEquals(e, test.getEbook());
+    }
+    @Test
+    void getAuthor() {
+        Assertions.assertEquals(a, b.getAuthor());
+    }
+
+    @Test
+    void getTitle() {
+        Assertions.assertEquals(t, b.getTitle());
+    }
+
+    @Test
+    void getPublisher() {
+        b.setPublisher(p);
+        Assertions.assertEquals(p, b.getPublisher());
+    }
+
+    @Test
+    void getEbook() {
+        Assertions.assertFalse(b.getEbook());
+        b.setEbook(true);
+        Assertions.assertTrue(b.getEbook());
+    }
+
+    @Test
+    void setIllegalAuthor() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> b.setAuthor(""));
+    }
+
+    @Test
+    void setAuthor() {
+        String s = "test";
+        b.setAuthor(s);
+        Assertions.assertEquals(s, b.getAuthor());
+    }
+
+    @Test
+    void setIllegalTitle() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> b.setTitle(""));
+    }
+
+    @Test
+    void setTitle() {
+        String s = "test";
+        b.setTitle(s);
+        Assertions.assertEquals(s, b.getTitle());
+    }
+
+    @Test
+    void setYear() {
+        b.setYear(y);
+        Assertions.assertEquals(y, b.getYear());
+    }
+
+    @Test
+    void setIsbn() {
+        b.setIsbn(i);
+        Assertions.assertEquals(i, b.getIsbn());
+    }
+
+    @Test
+    void setPrice() {
+        b.setPrice(price);
+        Assertions.assertEquals(price, b.getPrice());
+    }
+
+    @Test
+    void getAllSet() {
+        Book test = new Book(a, t, p, y, i, price, e);
+
+        String r = "Author: " + a + System.lineSeparator()
+                + "Title: " + t + System.lineSeparator()
+                + "Publisher: " + p + System.lineSeparator()
+                + "Year: " + y + System.lineSeparator()
+                + "ISBN: " + i + System.lineSeparator()
+                + "Price: " + price + System.lineSeparator()
+                + "Ebook: " + e;
+
+        Assertions.assertEquals(r, test.getAllSet());
+    }
+
+    @Test
+    void getString() {
+        String s1 = "test1";
+        String s2 = "test2";
+        String r = "Author: " + s1 + System.lineSeparator()
+                + "Title: " + s2 + System.lineSeparator();
+        b.setAuthor(s1);
+        b.setTitle(s2);
+
+        Assertions.assertEquals(r, b.toString());
+    }
+}
